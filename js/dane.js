9e1b705315a7a6f2d88cf72f25981c1347480a6a
Danealor <iamblonder@gmail.com>
2016-12-16 18:46:40 -0500

    Modernize and Awesomize

---
+++ js/dane.js
@@ -0,0 +1,23 @@
+﻿(function ($) {
+
+    var targetNodes = $("#nav");
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    var myObserver = new MutationObserver(mutationHandler);
+    var obsConfig = { childList: true, characterData: true, attributes: true, subtree: true };
+
+    //--- Add a target node to the observer. Can only add one node at a time.
+    targetNodes.each(function () {
+        myObserver.observe(this, obsConfig);
+    });
+
+    updateMargin();
+
+    function mutationHandler(mutationRecords) {
+        updateMargin();
+    }
+
+    function updateMargin() {
+        $("#body").attr('margin-top', $("#nav").attr('height'));
+    }
+
+}(jQuery));
